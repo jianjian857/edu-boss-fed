6cbb59cffcb26581e05aa81caf930d34e191616d
jianjian857 <jianjian@example.com>
2022-02-15 14:57:45 +0800

    fix:111

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入 Element 组件库
+import ElementUI from 'element-ui'
+// 引入 Element 主题文件
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 引入全局样式
+import './styles/index.scss'
+
+// 注册为 Vue 插件
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
